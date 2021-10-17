4905ae7fae9a8d9d57f6f0e63f10329fb56e578b
Christine Rowland <fordo29@gmail.com>
2021-10-17 02:23:31 -0600

    Submit library checkout book function

--- src/Library.js
+++ src/Library.js
@@ -15,8 +15,18 @@ function addBook(libraryName, book) {
       libraryName.shelves[book.genre].push(book);
   }
 
+function checkoutBook(libraryName, bookTitle, shelf) {
+  for (var i = 0; i < libraryName.shelves[shelf].length; i++) {
+    if (libraryName.shelves[shelf][i].title === bookTitle) {
+      libraryName.shelves[shelf].splice(i, 1)
+    return `You have now checked out ${bookTitle} from the ${libraryName.name}`
+  }
+  }
+  return `Sorry, there are currently no copies of ${bookTitle} available at the ${libraryName.name}`
+}
+
 module.exports = {
   createLibrary,
   addBook,
-  // checkoutBook
+  checkoutBook
 }
